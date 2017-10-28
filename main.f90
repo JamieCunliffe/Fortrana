b4f2a1cee07e918a17f744a4cfdc82ff37eb979f
Jamie Cunliffe <Jamie.Cunliffe@outlook.com>
2017-10-29 00:04:23 +0100

    Init the grid

--- main.f90
+++ main.f90
@@ -31,11 +31,14 @@ INTEGER :: KEY4=24
 INTEGER :: KEY5=25
 
 INTEGER :: SWORD=69
+
+INTEGER :: NRPOD = 665
+INTEGER :: POD = 666
 END MODULE consts
 
 
 MODULE Vars
-  INTEGER, DIMENSION(0:3,0:3) :: field
+  INTEGER, DIMENSION(0:8,0:8) :: field
   INTEGER currX
   INTEGER currY
 END MODULE Vars
@@ -45,29 +48,90 @@ INTEGER FUNCTION intRand(u)
   intRand = int(rand(0)*u)+1
 END FUNCTION intRand
 
-! ENUM, BIND(C) :: DIRECTIONS
-!  ENUMERATOR :: UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3 
-!END ENUM
-
-!ENUM, BIND(C) :: TILE
-!  ENUMERATOR :: EMPTY = 0, OB1 = 6, OB2 = 7, OB3 = 8, OB4 = 9, OB5 = 10, MON1 = 11, MON2 = 12, MON3 = 13, MON4 = 14, MON5 = 15,
-!  SWORD = 69, NRPOD = 665 POD = 666 
-!END ENUM
-
-!TYPE(DIRECTION) :: DIRECTION
 
 INTEGER FUNCTION init()
   USE Vars
-  field(0,0) = 0
-  field(0,1) = 1
-  field(0,2) = 2
-  field(1,0) = 3
-  field(1,1) = 4
-  field(1,2) = 5
-  field(2,0) = 6
-  field(2,1) = 7
-  field(2,2) = 8
+  USE consts
 
+  currX = 4
+  currY = 5
+  
+  field(0,0) = INPUT1
+  field(0,1) = KEY1
+  field(0,2) = TEMPTY
+  field(0,3) = MONSTER1
+  field(0,4) = TEMPTY
+  field(0,5) = INPUT2
+  field(0,6) = OBSTACLE1
+  field(0,7) = SWARD
+  
+    
+  field(1,0) = TEMPTY
+  field(1,1) = NRPOD
+  field(1,2) = NRPOD
+  field(1,3) = NRPOD
+  field(1,4) = TEMPTY
+  field(1,5) = TEMPTY
+  field(1,6) = OBSTACLE1
+  field(1,7) = TEMPTY
+
+    
+  field(2,0) = TEMPTY
+  field(2,1) = NRPOD
+  field(2,2) = POD
+  field(2,3) = NRPOD
+  field(2,4) = OBSTACLE1
+  field(2,5) = TEMPTY
+  field(2,6) = KEY4
+  field(2,7) = TEMPTY
+
+    
+  field(3,0) = TEMPTY
+  field(3,1) = NRPOD
+  field(3,2) = NRPOD
+  field(3,3) = NRPOD
+  field(3,4) = TEMPTY
+  field(3,5) = TEMPTY
+  field(3,6) = TEMPTY
+  field(3,7) = TEMPTY
+
+  field(4,0) = KEY5 
+  field(4,1) = TEMPTY
+  field(4,2) = INPUT4
+  field(4,3) = OBSTACLE1
+  field(4,4) = TEMPTY
+  field(4,5) = TEMPTY
+  field(4,6) = INPUT3
+  field(4,7) = TEMPTY
+
+  field(5,0) = MONSTER1
+  field(5,1) = TEMPTY
+  field(5,2) = KEY3
+  field(5,3) = OBSTACLE1
+  field(5,4) = TEMPTY
+  field(5,5) = TEMPTY
+  field(5,6) = TEMPTY
+  field(5,7) = TEMPTY
+
+  field(6,0) = TEMPTY
+  field(6,1) = TEMPTY
+  field(6,2) = TEMPTY
+  field(6,3) = TEMPTY
+  field(6,4) = KEY2
+  field(6,5) = TEMPTY
+  field(6,6) = TEMPTY
+  field(6,7) = TEMPTY
+
+  field(7,0) = INPUT5
+  field(7,1) = OBSTACLE1
+  field(7,2) = OBSTACLE1
+  field(7,3) = TEMPTY
+  field(7,4) = TEMPTY
+  field(7,5) = TEMPTY
+  field(7,6) = TEMPTY
+  field(7,7) = MONSTER4
+
+  
   init = 1
 END FUNCTION init
 
@@ -76,8 +140,8 @@ INTEGER FUNCTION move(dir)
   USE consts  
   INTEGER dir
   PRINT *, test
-  DO i = 0, 2, 1
-    DO j = 0, 2, 1
+  DO i = 0, 7, 1
+    DO j = 0, 7, 1
       PRINT *, field(i,j)
     END DO
   END DO
